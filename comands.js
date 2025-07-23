22ec7e887144ebe4a6ec69d449884e5735d500ea
furaoferrenho <scardozodacunha@gmail.com>
2025-07-23 16:44:45 -0300

    Add files via upload

--- comands.js
+++ comands.js
@@ -1,3 +1,23 @@
-window.open{
-alert('hi');
-};
+
+function comandos(){
+    var prox = document.getElementById('comando').value;
+var comander = prox.split(" ");
+switch(comander[0]){
+    case "calculo":
+     catalogo(comander[1], comander);
+    break;
+        default:
+   alert("oxi");
+        break;
+}
+}
+function catalogo(sites, comando){
+switch(sites){
+    case "fatores":
+        window.open("calculo fatores 1.1\\Fatores.html")
+        break;
+        case "pib":
+       window.open("https://furaoferrenho.github.io/ADM-2-C-sui-a-geo/")
+       break;
+}
+}
